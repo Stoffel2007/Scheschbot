9b00d76f9fb5017e111983db6ca22223cb854f91
zinkst56392 <zinkst56392@th-nuernberg.de>
2016-12-19 10:41:39 +0100

    neue Spalte in Tabelle "users": "like_percentage"

--- sql queries/create_table.sql
+++ sql queries/create_table.sql
@@ -2,8 +2,17 @@ CREATE TABLE IF NOT EXISTS users
 (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     telegram_id INT NOT NULL,
+    like_percentage INT NOT NULL DEFAULT 50,
     first_name CHAR(64) NOT NULL,
     last_name CHAR(64),
     username CHAR(64)
 )
 DEFAULT CHARACTER SET = 'utf16';
+
+INSERT INTO users
+(telegram_id, like_percentage, first_name)
+VALUES
+(123153, 30, 'Timo'),
+(123547, 90, 'Nusch'),
+(124895, 10, 'Frida'),
+(123521, 80, 'Andi')
