dc01d81b5f72b1b2e250b07904831c694eb5a434
zinkst56392 <zinkst56392@th-nuernberg.de>
2016-12-22 13:20:22 +0100

    Funktion insert() geschrieben

--- db_connect.py
+++ db_connect.py
@@ -78,3 +78,46 @@ def update(table, column_array, value_array, where_expression):
                    'WHERE ' + where_expression
 
     query(query_string)
+
+
+# INSERT-Abfrage auf der Datenbank
+def insert(table, column_array, value_array):
+    # überprüft, ob ein Objekt ein Integer ist
+    def is_int(number):
+        try:
+            int(number)
+            return True
+        except ValueError:
+            return False
+
+    # Fehler werfen, wenn die Anzahl an Spalten nicht mit der Anzahl an Werten übereinstimmt
+    if len(column_array) != len(value_array):
+        raise IndexError("Anzahl an Spalten und Wertem stimmt nicht überein")
+
+    print(value_array);
+    # Anführungszeichen bei Strings anfügen
+    for i in range(0, len(value_array)):
+        if not is_int(value_array[i]):
+            value_array[i] = '"' + value_array[i] + '"'
+
+    # Werte durch Kommata trennen
+    value_string = ''
+    for i in range(0, len(value_array)):
+        value_string += value_array[i].__str__()
+        # bei letztem Durchlauf kein Komma mehr setzen
+        if i < len(value_array) - 1:
+            value_string += ', '
+
+    # Spaltennamen durch Kommata trennen
+    column_string = ''
+    for i in range(0, len(column_array)):
+        column_string += column_array[i]
+        # bei letztem Durchlauf kein Komma mehr setzen
+        if i < len(column_array) - 1:
+            column_string += ', '
+
+    query_string = 'INSERT INTO ' + table + ' ' +\
+                   '(' + column_string + ') ' +\
+                   'VALUES (' + value_string + ')'
+
+    query(query_string)
