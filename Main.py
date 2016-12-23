e6e8a9514e6d7a8e9a4e39e91bcb95458dfff6b5
zinkst56392 <zinkst56392@th-nuernberg.de>
2016-12-23 15:34:40 +0100

    Kommentar zurechtgerückt

--- Main.py
+++ Main.py
@@ -33,9 +33,9 @@ def main():
                 # like_percentage des Users zufällig neu setzen
                 user = get_user(update)
                 like_percentage = random.randint(0, 100)
+                # like_percentage des Users in der Datenbank neu setzen
                 mood.set_mood(user, like_percentage)
 
-                # like_percentage des Users neu setzen in der Datenbank
                 last_update_id = update.update_id + 1
             if temp is not last_update_id:
                 print("last_update_id =", last_update_id)
