30f895effa035923535113a79138df11cdf9e95d
zinkst56392 <zinkst56392@th-nuernberg.de>
2016-12-23 14:27:20 +0100

    übersichtliche Leerzeile :3

--- db_connect.py
+++ db_connect.py
@@ -17,6 +17,7 @@ def __query(query_string):
         cursor.execute('SET NAMES utf8;')
         cursor.execute('SET CHARACTER SET utf8;')
         cursor.execute('SET character_set_connection=utf8;')
+
         start = time.time()
         cursor.execute(query_string)
         end = time.time()
