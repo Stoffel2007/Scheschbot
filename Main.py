5ba9ae29a3f90390bf9b4ce4f9dfb74dbd9a8bf2
zinkst56392 <zinkst56392@th-nuernberg.de>
2016-12-24 23:15:56 +0100

    Funktion send_reaction() geschrieben

--- Main.py
+++ Main.py
@@ -38,13 +38,17 @@ def main():
 
                 # Inline-Query abfragen
                 if update.inline_query and update.inline_query.query:
-                    bot.answer_inline_query(update.inline_query.id, answer_inline(update.inline_query))
+                    params_dict = {'inline_query_id': update.inline_query.id,
+                                   'results': get_inline_results(update.inline_query.query)}
+                    send_reaction(bot, 'inline', params_dict)
 
                 # Nachricht abfragen
                 if update.message and update.message.text == "/kochnudeln":
-                        bot.send_message(update.message.chat_id, "Ok, wird gemacht....")
-                        event = koch_nudeln(update.message)
-                        event_handler.add_event(event)
+                    params_dict = {'chat_id': update.message.chat_id,
+                                   'text': 'Ok, wird gemacht....'}
+                    send_reaction(bot, 'text', params_dict)
+                    event = koch_nudeln(update.message)
+                    event_handler.add_event(event)
 
                 # like_percentage des Users zufällig neu setzen
                 user = get_user(update)
@@ -59,7 +63,10 @@ def main():
             for event_update in event_handler.get_update():
                 message = event_update[0]
                 text = event_update[1]
-                bot.send_message(message.chat_id, text, reply_to_message_id=message.message_id)
+                params_dict = {'chat_id': message.chat_id,
+                               'text': text,
+                               'message_id': message.message_id}
+                send_reaction(bot, 'text', params_dict)
 
             if temp is not last_update_id:
                 print("last_update_id =", last_update_id)
@@ -94,8 +101,8 @@ def get_user(update):
         return update.inline_query.from_user
 
 
-def answer_inline(inline_query):
-    text = "Oh, hallo, Herr " + inline_query.query + "!"
+def get_inline_results(query_string):
+    text = "Oh, hallo, Herr " + query_string + "!"
     input_text = telegram.InputTextMessageContent(text)
     return [telegram.InlineQueryResultArticle('test_inline_query', "Mederer", input_text)]
 
@@ -106,6 +113,32 @@ def koch_nudeln(message):
                                  [5, "koche Nudeln für " + name + "...."],
                                  [5, "Nudeln für " + name + " sind fertig! :3"]])
     return kochnudeln
+
+
+def send_reaction(bot, action, params_dict):
+        if action == 'text':
+            print(params_dict.get('message_id', None))
+            bot.send_message(chat_id=params_dict['chat_id'],
+                             text=params_dict['text'],
+                             reply_to_message_id=params_dict.get('message_id', None))
+        elif action == 'photo':
+            bot.send_photo(chat_id=params_dict['chat_id'],
+                           text=params_dict['photo'],
+                           caption=params_dict['caption'],
+                           reply_to_message_id=params_dict.get('message_id', None))
+        elif action == 'gif':
+            bot.send_document(chat_id=params_dict['chat_id'],
+                              text=params_dict['gif'],
+                              caption=params_dict['caption'],
+                              reply_to_message_id=params_dict.get('message_id', None))
+        elif action == 'video':
+            bot.send_video(chat_id=params_dict['chat_id'],
+                           text=params_dict['gif'],
+                           caption=params_dict['caption'],
+                           reply_to_message_id=params_dict.get('message_id', None))
+        elif action == 'inline':
+            bot.answer_inline_query(inline_query_id=params_dict['inline_query_id'],
+                                    results=params_dict['results'])
 
 
 if __name__ == '__main__':
