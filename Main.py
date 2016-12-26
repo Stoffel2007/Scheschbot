c0526e15359cdbe3a2431d9999bac0ffb4524440
zinkst56392 <zinkst56392@th-nuernberg.de>
2016-12-26 23:00:59 +0100

    Verarbeiten von Updates ausgelagert

--- Main.py
+++ Main.py
@@ -1,12 +1,9 @@
+import time
 import telegram
-import aggronyme
 import constants
 import db_connect
-import mood
-import time
-import random
+import process_update
 from Events.EventHandler import *
-from Events.Event import Event
 
 
 def main():
@@ -35,82 +32,13 @@ def main():
         # alle Updates seit letztem Update holen
         try:
             for update in bot.getUpdates(offset=last_update_id):
-                # Update-Objekt mit allen Attributen wie in der Bot-API beschrieben (core.telegram.org/bots/api#update)
-                print("\tupdate =", update)
-
-                # Inline-Query abfragen
-                if update.inline_query and update.inline_query.query:
-                    params_dict = {'inline_query_id': update.inline_query.id,
-                                   'results': get_inline_results(update.inline_query.query)}
-                    send_reaction(bot, 'inline', params_dict)
-
-                # Nachricht abfragen
-                if update.message and update.message.text.startswith("/kochnudeln"):
-                    params_dict = {'chat_id': update.message.chat_id,
-                                   'text': 'Ok, wird gemacht....'}
-                    send_reaction(bot, 'text', params_dict)
-                    event = koch_nudeln(update.message)
-                    event_handler.add_event(event)
-
-                # Nachricht abfragen
-                if update.message and update.message.text.startswith("/aggro"):
-                    params = update.message.text.split(' ', 1)[1]
-                    message = aggronyme.aggro(params)
-                    params_dict = {'chat_id': update.message.chat_id,
-                                   'text': message}
-                    send_reaction(bot, 'text', params_dict)
-
-                # Inline-Button wurde gedrückt
-                if update.callback_query:
-                    params_dict = {'chat_id': update.callback_query.message.chat_id,
-                                   'message_id': update.callback_query.message.message_id}
-
-                    data_array = update.callback_query.data.split(' ', 1)
-                    params = data_array[1]
-                    if data_array[0] == 'yes':
-                        if aggronyme.insert_words(params) is True:
-                            params_dict['text'] = 'legg0 :3\n' \
-                                                  '"' + params + '" erfolgreich zur Datenbank hinzugefügt'
-                        else:
-                            # Artikel rausnehmen, falls vorhanden
-                            if params.startswith('der ') or params.startswith('die ') or params.startswith('die '):
-                                word = params.split(' ', 1)[1]
-                            else:
-                                word = params
-
-                            vote_incremented = aggronyme.increment_vote(word)
-                            if not vote_incremented:
-                                print('\tkonnte Votes von ' + word + ' nicht inkrementieren')
-                            params_dict['text'] = '"' + params + '"  konnte nicht zur Datenbank hinzugefügt werden\n' +\
-                                                  'Eventuell ist dieses Wort bereits vorhanden'
-                    elif data_array[0] == 'no':
-                        params_dict['text'] = '"' + params + '" abgelehnt'
-
-                    send_reaction(bot, 'edit_message', params_dict)
-
-                # Nachricht abfragen
-                if update.message and update.message.text.startswith("/feedme"):
-                    params = update.message.text.split(' ', 1)[1]
-                    button1 = telegram.InlineKeyboardButton('Akzeptieren', callback_data='yes ' + params)
-                    button2 = telegram.InlineKeyboardButton('Ablehnen', callback_data='no ' + params)
-                    keyboard = telegram.InlineKeyboardMarkup([[button1, button2]])
-
-                    # User-IDs von Alexey, Andi, Stoffel
-                    admin_ids = db_connect.select('users', 'telegram_id, first_name', where_expression='is_admin = 1')
-
-                    for i in range(len(admin_ids)):
-                        params_dict = {'chat_id': admin_ids[i][0],
-                                       'text': '"' + params + '" akzeptieren oder ablehnen?',
-                                       'reply_markup': keyboard}
-                        try:
-                            send_reaction(bot, 'text', params_dict)
-                        except telegram.error.Unauthorized:
-                            print('\t' + admin_ids[i][1].__str__() + ' hat den Bot nicht gestartet')
-
-                # like_percentage des Users zufällig neu setzen
-                user = get_user(update)
-                like_percentage = random.randint(0, 100)
-                mood.set_mood(user, like_percentage)
+                message_list = process_update.get_messages(update, event_handler)
+                for message in message_list:
+                    try:
+                        send_reaction(bot, message['action'], message['params_dict'])
+                    except telegram.error.Unauthorized:
+                        print('\tEin Benutzer (ID: ' + message['params_dict']['chat_id'].__str__() +
+                              ' hat den Bot nicht gestartet')
 
                 # Update-ID hochzählen
                 last_update_id = update.update_id + 1
@@ -140,36 +68,9 @@ def get_last_update_id(bot):
             return bot.getUpdates()[-1].update_id
         except IndexError:  # falls keine Updates
             return None
-        except telegram.error.NetworkError as error:
+        except telegram.error.NetworkError:
             print('\tVerbindung zum Bot fehlgeschlagen. Nächster Versuch in 10 Sekunden....')
             time.sleep(10)
-
-
-# User-Objekt aus dem Update-Objekt holen
-# je nach Update-Art (Nachricht, Inline Query, usw. liegt das User-Objekt in einem anderen Teil des Update-Objekts
-def get_user(update):
-    if update.message:  # wenn eine Nachricht gesendet wurde
-        return update.message.from_user
-    if update.edited_message:  # wenn eine Nachricht bearbeitet wurde
-        return update.edited_message.from_user
-    if update.inline_query:  # wenn eine Inline Query bearbeitet wurde
-        return update.inline_query.from_user
-    if update.callback_query:  # wenn eine Inline Query bearbeitet wurde
-        return update.callback_query.from_user
-
-
-def get_inline_results(query_string):
-    text = "Oh, hallo, Herr " + query_string + "!"
-    input_text = telegram.InputTextMessageContent(text)
-    return [telegram.InlineQueryResultArticle('test_inline_query', "Mederer", input_text)]
-
-
-def koch_nudeln(message):
-    name = message.from_user.first_name
-    kochnudeln = Event(message, [[5, "koche Wass0 für " + name + "...."],
-                                 [5, "koche Nudeln für " + name + "...."],
-                                 [5, "Nudeln für " + name + " sind fertig! :3"]])
-    return kochnudeln
 
 
 def send_reaction(bot, action, params_dict):
@@ -203,7 +104,7 @@ def send_reaction(bot, action, params_dict):
                                   text=params_dict['text'])
         else:
             print('\tfalsche action-Variable (' + action + ')')
-    except KeyError as error:
+    except KeyError:
         print('\tFehler im Parameter-Array:')
         print('\t', end='')
         print(params_dict)
---
+++ process_update.py
@@ -0,0 +1,115 @@
+import random
+import telegram
+import aggronyme
+import db_connect
+import mood
+from Events.Event import Event
+
+
+def get_messages(update, event_handler):
+    # Update-Objekt mit allen Attributen wie in der Bot-API beschrieben (core.telegram.org/bots/api#update)
+    print("\tupdate =", update)
+
+    # like_percentage des Users zufällig neu setzen
+    user = get_user(update)
+    like_percentage = random.randint(0, 100)
+    mood.set_mood(user, like_percentage)
+
+    # Inline-Query abfragen
+    if update.inline_query and update.inline_query.query:
+        params_dict = {'inline_query_id': update.inline_query.id,
+                       'results': get_inline_results(update.inline_query.query)}
+        return [{'action': 'inline', 'params_dict': params_dict}]
+
+    # Nachricht abfragen
+    if update.message and update.message.text.startswith("/kochnudeln"):
+        params_dict = {'chat_id': update.message.chat_id,
+                       'text': 'Ok, wird gemacht....'}
+        event = koch_nudeln(update.message)
+        event_handler.add_event(event)
+        return [{'action': 'text', 'params_dict': params_dict}]
+
+    # Nachricht abfragen
+    if update.message and update.message.text.startswith("/aggro"):
+        params = update.message.text.split(' ', 1)[1]
+        message = aggronyme.aggro(params)
+        params_dict = {'chat_id': update.message.chat_id,
+                       'text': message}
+        return [{'action': 'text', 'params_dict': params_dict}]
+
+    # Inline-Button wurde gedrückt
+    if update.callback_query:
+        params_dict = {'chat_id': update.callback_query.message.chat_id,
+                       'message_id': update.callback_query.message.message_id}
+
+        data_array = update.callback_query.data.split(' ', 1)
+        params = data_array[1]
+        if data_array[0] == 'yes':
+            if aggronyme.insert_words(params) is True:
+                params_dict['text'] = 'legg0 :3\n' \
+                                      '"' + params + '" erfolgreich zur Datenbank hinzugefügt'
+            else:
+                # Artikel rausnehmen, falls vorhanden
+                if params.startswith('der ') or params.startswith('die ') or params.startswith('die '):
+                    word = params.split(' ', 1)[1]
+                else:
+                    word = params
+
+                vote_incremented = aggronyme.increment_vote(word)
+                if not vote_incremented:
+                    print('\tkonnte Votes von ' + word + ' nicht inkrementieren')
+                params_dict['text'] = '"' + params + '"  konnte nicht zur Datenbank hinzugefügt werden\n' + \
+                                      'Eventuell ist dieses Wort bereits vorhanden'
+        elif data_array[0] == 'no':
+            params_dict['text'] = '"' + params + '" abgelehnt'
+
+        return [{'action': 'edit_message', 'params_dict': params_dict}]
+
+    # Nachricht abfragen
+    if update.message and update.message.text.startswith("/feedme"):
+        params = update.message.text.split(' ', 1)[1]
+        button1 = telegram.InlineKeyboardButton('Akzeptieren', callback_data='yes ' + params)
+        button2 = telegram.InlineKeyboardButton('Ablehnen', callback_data='no ' + params)
+        keyboard = telegram.InlineKeyboardMarkup([[button1, button2]])
+
+        # User-IDs von Alexey, Andi, Stoffel
+        admin_ids = db_connect.select('users', 'telegram_id', where_expression='is_admin = 1')
+
+        message_list = []
+
+        for i in range(len(admin_ids)):
+            params_dict = {'chat_id': admin_ids[i][0],
+                           'text': '"' + params + '" akzeptieren oder ablehnen?',
+                           'reply_markup': keyboard}
+            message_list.append({'action': 'text', 'params_dict': params_dict})
+
+        return message_list
+
+    return []
+
+
+# User-Objekt aus dem Update-Objekt holen
+# je nach Update-Art (Nachricht, Inline Query, usw. liegt das User-Objekt in einem anderen Teil des Update-Objekts
+def get_user(update):
+    if update.message:  # wenn eine Nachricht gesendet wurde
+        return update.message.from_user
+    if update.edited_message:  # wenn eine Nachricht bearbeitet wurde
+        return update.edited_message.from_user
+    if update.inline_query:  # wenn eine Inline Query bearbeitet wurde
+        return update.inline_query.from_user
+    if update.callback_query:  # wenn eine Inline Query bearbeitet wurde
+        return update.callback_query.from_user
+
+
+def get_inline_results(query_string):
+    text = "Oh, hallo, Herr " + query_string + "!"
+    input_text = telegram.InputTextMessageContent(text)
+    return [telegram.InlineQueryResultArticle('test_inline_query', "Mederer", input_text)]
+
+
+def koch_nudeln(message):
+    name = message.from_user.first_name
+    kochnudeln = Event(message, [[5, "koche Wass0 für " + name + "...."],
+                                 [5, "koche Nudeln für " + name + "...."],
+                                 [5, "Nudeln für " + name + " sind fertig! :3"]])
+    return kochnudeln
