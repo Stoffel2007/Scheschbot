c8a585ccf2b97ea24a60f7eb300aea9ee111e2b5
zinkst56392 <zinkst56392@th-nuernberg.de>
2017-01-02 16:32:00 +0100

    INSERT-Befehl entfernt (enthält Nutzerdaten und Daten ändern sich sowieso dauernd) => Stoffel schickt einfach die .sql-Datei für die komplette-Datei rum

--- sql queries/create_database.sql
+++ sql queries/create_database.sql
@@ -1,2 +1,44 @@
 CREATE DATABASE IF NOT EXISTS scheschbot
 DEFAULT CHARACTER SET = 'utf16';
+
+
+CREATE TABLE IF NOT EXISTS scheschbot.users
+(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    telegram_id INT NOT NULL,
+    like_percentage INT NOT NULL DEFAULT 50,
+    first_name CHAR(64) NOT NULL,
+    last_name CHAR(64),
+    username CHAR(64),
+    is_admin BOOLEAN NOT NULL DEFAULT 0
+)
+DEFAULT CHARACTER SET = 'utf16'
+
+
+CREATE TABLE IF NOT EXISTS scheschbot.word_types
+(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name CHAR(64) NOT NULL
+)
+DEFAULT CHARACTER SET = 'utf16'
+
+
+CREATE TABLE IF NOT EXISTS scheschbot.genus
+(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    article CHAR(3) NOT NULL
+)
+DEFAULT CHARACTER SET = 'utf16'
+
+
+CREATE TABLE IF NOT EXISTS scheschbot.aggronymes
+(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    word CHAR(64) NOT NULL,
+    type_id INT NOT NULL,
+    genus_id INT,
+    votes INT NOT NULL DEFAULT 1,
+    FOREIGN KEY (type_id) REFERENCES scheschbot.word_types (id),
+    FOREIGN KEY (genus_id) REFERENCES scheschbot.genus (id)
+)
+DEFAULT CHARACTER SET = 'utf16'
