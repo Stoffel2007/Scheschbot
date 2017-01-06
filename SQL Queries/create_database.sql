8de54e10bfa0a66b03ecde5f292135162636d177
zinkst56392 <zinkst56392@th-nuernberg.de>
2017-01-06 13:08:04 +0100

    ON DELETE CASCADE löscht Einträge in Tabellen, die auf gelöschte Objekte in anderen Tabellen verweisen

--- SQL Queries/create_database.sql
+++ SQL Queries/create_database.sql
@@ -38,7 +38,40 @@ CREATE TABLE IF NOT EXISTS scheschbot.aggronymes
     type_id INT NOT NULL,
     genus_id INT,
     votes INT NOT NULL DEFAULT 1,
-    FOREIGN KEY (type_id) REFERENCES scheschbot.word_types (id),
-    FOREIGN KEY (genus_id) REFERENCES scheschbot.genus (id)
+    FOREIGN KEY (type_id) REFERENCES scheschbot.word_types (id) ON DELETE CASCADE,
+    FOREIGN KEY (genus_id) REFERENCES scheschbot.genus (id) ON DELETE CASCADE
+)
+DEFAULT CHARACTER SET = 'utf16'
+
+
+CREATE TABLE IF NOT EXISTS scheschbot.answer_input
+(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    input CHAR(64) NOT NULL,
+    text_before BOOLEAN NOT NULL DEFAULT 0,
+    text_after BOOLEAN NOT NULL DEFAULT 0,
+    is_question BOOLEAN NOT NULL DEFAULT 0,
+    contains_specialchars BOOLEAN NOT NULL DEFAULT 0
+)
+DEFAULT CHARACTER SET = 'utf16'
+
+
+CREATE TABLE IF NOT EXISTS scheschbot.answer_output
+(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    output CHAR(64) NOT NULL
+)
+DEFAULT CHARACTER SET = 'utf16'
+
+
+CREATE TABLE IF NOT EXISTS scheschbot.answer_relations
+(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    input_id INT NOT NULL,
+    output_id INT NOT NULL,
+    previous_output_id INT DEFAULT NULL,
+    FOREIGN KEY (input_id) REFERENCES scheschbot.answer_input (id) ON DELETE CASCADE,
+    FOREIGN KEY (output_id) REFERENCES scheschbot.answer_output (id) ON DELETE CASCADE,
+    FOREIGN KEY (previous_output_id) REFERENCES scheschbot.answer_output (id) ON DELETE CASCADE
 )
 DEFAULT CHARACTER SET = 'utf16'
