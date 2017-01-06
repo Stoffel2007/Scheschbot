c885f59eaa160a47dadcbe4bcf42378bcf056404
Stefan Zink <stoffel.zink@t-online.de>
2017-01-06 11:29:10 +0100

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,10 @@
+# Botdaten
+botkey = '149056495:AAEMFbT7Y3YS7jlkkdZG0JSm_fXBqcj-Epw'  # Scheschbot
+botname = 'scheschbot'
+
+# Datenbankverbindung
+host = 'localhost'
+port = 3306
+user = 'root'
+passwd = ''
+db = 'scheschbot'
