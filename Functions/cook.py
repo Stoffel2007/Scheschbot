b48c8bb7ea817653f282821d1388e336e6775058
zinkst56392 <zinkst56392@th-nuernberg.de>
2017-01-07 20:20:03 +0100

    Default-Wert, falls kein Gericht angegeben wurde

--- Functions/cook.py
+++ Functions/cook.py
@@ -2,11 +2,14 @@ from Events.Event import Event
 
 
 def cook(message, food, event_handler):
-    name = message.from_user.first_name
-    food = food.title()  # auf Großschreibung achten
-    events = Event(message, [[5, 'koche Wass0 für ' + name + '....'],
-                             [5, 'koche ' + food + ' für ' + name + '....'],
-                             [5, food + ' für ' + name + ' sind fertig! :3']])
-    event_handler.add_event(events)
+    if food != '':
+        name = message.from_user.first_name
+        food = food.title()  # auf Großschreibung achten
+        events = Event(message, [[5, 'koche Wass0 für ' + name + '....'],
+                                 [5, 'koche ' + food + ' für ' + name + '....'],
+                                 [5, food + ' für ' + name + ' sind fertig! :3']])
+        event_handler.add_event(events)
 
-    return 'Ok, wird gemacht....'
+        return 'Ok, wird gemacht....'
+    # falls kein Gericht angegeben wurde
+    return 'Was soll ich denn bitte kochen?! Das musst du schon dazusagen, du Trottel!'
