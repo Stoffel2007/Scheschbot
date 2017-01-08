ecb5f728398ab7431de1ac599340d2f90a1420a7
zinkst56392 <zinkst56392@th-nuernberg.de>
2017-01-08 21:55:25 +0100

    auf 'moin' mit einer zur Uhrzeit passenden Nachricht antworten

--- Functions/answer_special.py
+++ Functions/answer_special.py
@@ -1,2 +1,29 @@
+import time
+
+from Util import StringUtils
+
+
 def get_answer(message):
-    return ''
+    answer = ''
+
+    # Sonderzeichen abschneiden
+    normalized_message = StringUtils.cut_specialchars(message)
+
+    if normalized_message.startswith('moin ') or normalized_message == 'moin':
+        answer = __get_greeting()
+
+    return answer
+
+
+# liefert je nach Uhrzeit eine andere Antwort zurück
+def __get_greeting():
+    greetings_array = [['Nix "moin"! Es ist mitten in der Nacht, du Vollidiot!', 5],
+                       ['Was treibt dich denn bitte um zu dieser unchritlichen Stunde?!', 8],
+                       ['Moin, du Önn0 :3', 12],
+                       ['Was für "moin"?! Es ist hellichter Tag, du Langschläfer!', 16],
+                       ['Was hast du denn für einen Schlafrhythmus, dass du erst jetzt aufstehst?!', 20],
+                       ['Geh mal lieber önnen, es ist spätabends, du Depp!', 24]]
+    hour = time.gmtime().tm_hour
+    for greeting in greetings_array:
+        if hour < greeting[1]:
+            return greeting[0]
